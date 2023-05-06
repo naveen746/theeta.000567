0b1b5f3a080bb53222995073288706e2be6944ee
naveen746 <113990780+naveen746@users.noreply.github.com>
2023-05-06 19:17:18 +0530

    exception got added

--- dh.py
+++ dh.py
@@ -1,4 +1,12 @@
- dh = 90
- th = 180
- f = dh + th
- print('f',"=",f)
+dh = 90
+th = 180
+f = dh + th
+print('f',"=",f)
+
+try :
+ j = dh / 0
+ print(j)
+except ZeroDivisionError :
+ print('the integer cannot divided by zero')
+finally 
+print('got executed')
